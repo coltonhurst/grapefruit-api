4ac3c832916150d114729234f39bc28ac93f8ac6
Colton Hurst <colton@coltonhurst.com>
2021-06-29 08:06:50 -0400

    Add basic member routes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[get("/v1/member/<guid>")]
+fn get_member(guid: &str) -> String {
+    format!("Return the member with guid id: {}", guid)
+}
+
+#[post("/v1/member/<username>")]
+fn post_member(username: &str) -> String {
+    format!("Creating the member with username: {}", username)
+}
+
+#[put("/v1/member/<guid>")]
+fn put_member(guid: &str) -> String {
+    format!("Updating the member with guid: {}", guid)
+}
+
+#[delete("/v1/member/<guid>")]
+fn delete_member(guid: &str) -> String {
+    format!("Deleting the member with guid: {}", guid)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![get_member, post_member, put_member, delete_member])
 }
