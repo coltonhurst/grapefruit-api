c920a8a07f3170f1c3303248faa0032c80bb20b7
coltonhurst <colton@coltonhurst.com>
2021-08-08 20:27:46 -0400

    Add create_post to available routes

--- src/main.rs
+++ src/main.rs
@@ -399,7 +399,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 login,
                 put_member,
                 get_posts,
-                //create_post,
+                create_post,
             ],
         )
         .attach(cors)
